b236ca2c555407388dd6f9063cf7c32c7007fdd5
vdegenne <vdegenne@gmail.com>
2023-03-08 18:11:16 +0100

    add target option

--- background.js
+++ background.js
@@ -1,6 +1,29 @@
+// Set default values
+chrome.runtime.onInstalled.addListener(async function() {
+	const targetValue = await getTargetValue()
+
+    if (targetValue == undefined) {
+      // Set the default value for the target
+      chrome.storage.sync.set({ target: 'https://github.com/' }, function() {
+        // console.log('Default value is set');
+      });
+    }
+});
+
+
 chrome.action.onClicked.addListener(async () => {
+  const targetValue = await getTargetValue()
   await chrome.tabs.create({
-    url: 'https://github.com/',
+    url: targetValue ?? 'https://github.com/',
     active: true
   })
 });
+
+
+function getTargetValue() {
+	return new Promise((res) => {
+		chrome.storage.sync.get(['target'], function (result) {
+			res(result.target)
+		})
+	})
+}
