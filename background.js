ea2dedf1a402feae1a3ca8464272fb2f6e05bac2
Valentin Degenne <vdegenne@gmail.com>
2025-05-31 09:14:09 +0200

    shortcut

--- background.js
+++ background.js
@@ -1,29 +1,38 @@
-// Set default values
-chrome.runtime.onInstalled.addListener(async function() {
-	const targetValue = await getTargetValue()
-
-    if (targetValue == undefined) {
-      // Set the default value for the target
-      chrome.storage.sync.set({ target: 'https://github.com/' }, function() {
-        // console.log('Default value is set');
-      });
-    }
-});
-
-
-chrome.action.onClicked.addListener(async () => {
-  const targetValue = await getTargetValue()
-  await chrome.tabs.create({
-    url: targetValue ?? 'https://github.com/',
-    active: true
-  })
-});
-
-
 function getTargetValue() {
 	return new Promise((res) => {
 		chrome.storage.sync.get(['target'], function (result) {
-			res(result.target)
-		})
-	})
+			res(result.target);
+		});
+	});
 }
+
+async function openLink() {
+	const targetValue = await getTargetValue();
+	await chrome.tabs.create({
+		url: targetValue ?? 'https://github.com/',
+		active: true,
+	});
+}
+
+chrome.runtime.onInstalled.addListener(async function () {
+	const targetValue = await getTargetValue();
+
+	if (targetValue == undefined) {
+		// Set the default value for the target
+		chrome.storage.sync.set(
+			{target: 'https://github.com/vdegenne?tab=repositories'},
+			function () {
+				// console.log('Default value is set');
+			}
+		);
+	}
+});
+
+chrome.action.onClicked.addListener(async () => {
+	openLink();
+});
+chrome.commands.onCommand.addListener(function (command) {
+	if (command === 'open-link') {
+		openLink();
+	}
+});
--- dev/options/options.js
+++ dev/options/options.js
@@ -1,29 +1,25 @@
 import '@material/mwc-textfield';
 
-const textfield = document.querySelector('mwc-textfield')
+const textfield = document.querySelector('mwc-textfield');
 
-	textfield.addEventListener('change', (e) => {
-	const target = e.target
+textfield.addEventListener('change', (e) => {
+	const target = e.target;
 	// if (target.value) {
-		// localStorage.setItem('field-value', target.value)
-		saveValue(target.value)
+	// localStorage.setItem('field-value', target.value)
+	saveValue(target.value);
 	// }
-
-
-})
-
-;(async function() {
-	textfield.value = await getTargetValue()
+});
+(async function () {
+	textfield.value = await getTargetValue();
 })();
-
 
 function getTargetValue() {
 	return new Promise((res) => {
 		chrome.storage.sync.get(['target'], function (options) {
-			res(options.target)
-		})
-	})
+			res(options.target);
+		});
+	});
 }
 function saveValue(value) {
-	chrome.storage.sync.set({ target: value })
+	chrome.storage.sync.set({target: value});
 }
